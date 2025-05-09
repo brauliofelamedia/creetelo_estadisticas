5ce50e44e5227e20d30ba180cb086b9b8f49560f
Braulio Miramontes <braulio@felamedia.com>
2025-05-08 18:28:56 -0700

    Se hacen cambios en los calculos

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -298,21 +298,6 @@
                 </div>
                 </div>
             </div>
-            <div class="col">
-                <div class="card shadow-none border bg-gradient-start-5 h-100">
-                <div class="card-body p-20">
-                    <div class="d-flex flex-wrap align-items-center justify-content-between gap-3">
-                        <div>
-                            <p class="fw-medium text-primary-light mb-1">Mejor mes - {{ isset($bestMonth['month']) ? $bestMonth['month'] : 'N/A' }} {{ isset($bestMonth['year']) ? $bestMonth['year'] : '' }}</p>
-                            <h6 class="mb-0">$ {{number_format(@$bestMonth['amount'],0)}} USD</h6>
-                        </div>
-                        <div class="w-50-px h-50-px bg-red rounded-circle d-flex justify-content-center align-items-center">
-                            <iconify-icon icon="fa6-solid:file-invoice-dollar" class="text-white text-2xl mb-0"></iconify-icon>
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </div>
             <!-- Nueva tarjeta para el ticket promedio -->
             <div class="col">
                 <div class="card shadow-none border bg-gradient-start-3 h-100">
